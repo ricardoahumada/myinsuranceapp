0a4df8c74c5a0069a1ddbc4137c0ec2b4129107b
gahymadfer <ahumadag08@gmail.com>
2022-06-13 10:56:41 +0200

    añadido log in y dashbaord

--- project/controllers/web/__init__.py
+++ project/controllers/web/__init__.py
@@ -12,3 +12,10 @@ __all__ = [os.path.basename(
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
