84058517ec2b2f75382a5caf633f37e08b788a06
gahymadfer <ahumadag08@gmail.com>
2022-06-13 15:50:17 +0200

    log in empiezo

--- project/controllers/web/__init__.py
+++ project/controllers/web/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 __version__ = '0.1'
-
 from project import app
 from flask import render_template, request, url_for, flash, redirect
 import os
 import glob
+from flask import Flask, render_template, redirect, url_for, request
+
 __all__ = [os.path.basename(
     f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
 
@@ -12,10 +13,18 @@ __all__ = [os.path.basename(
 def index():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
