5e2e8d19cc4462a102fa174eae4fd81a3323010b
ricardonetmind <ricardoag@expert.netmind.es>
2022-07-22 21:10:57 +0200

    update tests

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -5,8 +5,34 @@ from project import app
 
 class TestApp(unittest.TestCase):
     token=''
-
-    def test_get_user_products(self):
-        return None
+    
+    def test_1_getToken(self):
+        tester = app.test_client(self)
+        test_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
+        response = tester.post('/api/v1/token',content_type='application/json', json=test_data)        
+        data=json.loads(response.text)
+        print(f"post token: {data}")
+        self.assertEqual(response.status_code, 200)
+        if response.status_code==200:
+            TestApp.token=data['token']
+        
+    def test_2_get_user_products_valid_token(self):
+        tester = app.test_client(self)
+        print(f"token: {self.token}")
+        headers = {"Authorization": f"Bearer {TestApp.token}"}
+        response = tester.get('/api/v1/users/1/products', content_type='application/json', headers=headers)
+        data=json.loads(response.text)
+        print(f"get_user_products: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(data)>0)
+    
+    def test_3_get_user_products_invalid_token(self):
+        tester = app.test_client(self)
+        ivalid_fake_token='CfDJ8OW5OI0CPGJBgSNlGwO0x4YF7qbYKVv7KOO-N0eFtDUzXOrL7F9Xd9W1otVi4ueJOkAmAhuoHFWNkqRaFD7zvAMHMSKncl6Vo5QXKmpvy6vqxOKxSURdIey8aZPRi3Nnhp2p9la-Al5xrVKz0lignRdcCHf3O7pF9zv_sNx_c_T7pUe3WsxaJEPX3t_9FO2Wjw'
+        headers = {"Authorization": f"Bearer {ivalid_fake_token}"}
+        response = tester.get('/api/v1/users/1/products', content_type='application/json', headers=headers)
+        data=json.loads(response.text)
+        print(f"get_user_products: {data}")
+        self.assertTrue(response.status_code > 400)
 
     
--- tests/acceptance-request/test_app_request.py
+++ tests/acceptance-request/test_app_request.py
@@ -6,4 +6,31 @@ class TestApp(unittest.TestCase):
     token=''
     base_url='http://localhost:5000/api/v1'
 
-    
+    def test_1_getToken(self):
+        url=f"{self.base_url}/token"
+        test_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
+        response = requests.post(url, json=test_data)
+        data=json.loads(response.text)
+        print(f"post: {data}")
+        self.assertEqual(response.status_code, 200)
+        if response.status_code==200:
+            TestApp.token=data['token']
+
+    def test_2_get_user_products_valid_token(self):
+        url=f"{self.base_url}/users/1/products"
+        print(f"token: {self.token}")
+        headers = {"Authorization": f"Bearer {TestApp.token}"}
+        response = requests.get(url, headers=headers)
+        data=json.loads(response.text)
+        print(f"get_user_products: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(data)>0)
+
+    def test_3_get_user_products_invalid_token(self):
+        url=f"{self.base_url}/users/1/products"
+        invalid_fake_token='CfDJ8OW5OI0CPGJBgSNlGwO0x4YF7qbYKVv7KOO-N0eFtDUzXOrL7F9Xd9W1otVi4ueJOkAmAhuoHFWNkqRaFD7zvAMHMSKncl6Vo5QXKmpvy6vqxOKxSURdIey8aZPRi3Nnhp2p9la-Al5xrVKz0lignRdcCHf3O7pF9zv_sNx_c_T7pUe3WsxaJEPX3t_9FO2Wjw'
+        headers = {"Authorization": f"Bearer {invalid_fake_token}"}
+        response = requests.get(url, headers=headers)
+        data=json.loads(response.text)
+        print(f"get_user_products: {data}")
+        self.assertTrue(response.status_code > 400)
